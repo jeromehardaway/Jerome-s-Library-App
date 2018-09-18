f0e07134f2898cf19cbae5fcba9a4679b4ca3f7d
Jerome Hardaway <jhard0205@jhard0205mbpx2.local>
2018-09-18 16:57:10 -0500

    Added action to inviation button

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,16 +1,21 @@
-import { computed, observer } from '@ember/object';
 import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
 
+	responseMessage: '',
 	emailAddress: '',
 
-	actualEmailAddress: computed('emailAddress', function() {
-		console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-	}),
+	isValid: match('emailAddress', /^.+@.+\..+$/),
+	isDisabled: not('isValid'),
 
-	emailAddressChanged: observer('emailAddress', function() {
-		console.log('observer is called', this.get('emailAddress'));
-	})
+	actions: {
+
+		saveInvitation() {
+			alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+			this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+			this.set('emailAddress', '');
+		}
+	}
 
 });
