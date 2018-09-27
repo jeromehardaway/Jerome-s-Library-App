b1ec4b4304640ae2c07eb51ad6aaa2818225da57
Jerome Hardaway <jhard0205@jhard0205mbpx2.lan>
2018-09-26 20:56:11 -0500

    Promise and the this context in javascript (+ playing with ES5 and ES6 a little)

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,28 +1,29 @@
-import Controller from '@ember/controller';
-import { match, not } from '@ember/object/computed';
+import Controller from "@ember/controller";
+import { match, not } from "@ember/object/computed";
 
 export default Controller.extend({
-  headerMessage: "Coming Soon...",
+  headerMessage: "Coming Soon",
   responseMessage: "",
   emailAddress: "",
 
   isValid: match("emailAddress", /^.+@.+\..+$/),
   isDisabled: not("isValid"),
 
-
   actions: {
     saveInvitation() {
-      const email = this.get('emailAddress');
+      const email = this.get("emailAddress");
 
-      const newInvitation = this.store.createRecord('invitation', {email: email });
-      newInvitation.save();
-      this.set(
-        "responseMessage",
-        `Thank you! We've just saved your email address: ${this.get(
-          "emailAddress"
-        )}`
-      );
-      this.set("emailAddress", "");
+      const newInvitation = this.store.createRecord("invitation", { email });
+
+      newInvitation.save().then(response => {
+        this.set(
+          "responseMessage",
+          `Thank you! We saved your email address with the following id: ${response.get(
+            "id"
+          )}`
+        );
+        this.set("emailAddress", "");
+      });
     }
   }
 });
